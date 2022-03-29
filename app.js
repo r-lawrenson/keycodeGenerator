62171f4459d1382c14499fe5e4ee89b6aceb0c5a
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-29 16:11:30 +0100

    added key number

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 document.addEventListener("keydown", (event) => {
 
+    let keyNumber = document.getElementById("keyNumber");
     let codeDisplay = document.getElementById("codeDisplay");
     let keyDisplay = document.getElementById("keyDisplay");
     let whichDisplay = document.getElementById("whichDisplay");
 
+    keyNumber.innerHTML = `${event.keyCode}`
     codeDisplay.innerHTML = `${event.code}`;
     keyDisplay.innerHTML = `${event.key}`;
     whichDisplay.innerHTML = `${event.which}`;
-    
+
 })
