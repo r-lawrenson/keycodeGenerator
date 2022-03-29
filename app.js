1853f765fec011ef4086c373350cfcc309572889
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-29 15:57:09 +0100

    addded key display event

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener("keydown", (event) => {
 
     let codeDisplay = document.getElementById("codeDisplay");
+    let keyDisplay = document.getElementById("keyDisplay");
 
     codeDisplay.innerHTML = `${event.code}`;
+    keyDisplay.innerHTML = `${event.key}`;
 })
