fc206b4b55fda85cdcb648366b110d473ca9c434
Rob Lawrenson <roblawrenson@hotmail.co.uk>
2022-03-29 16:05:58 +0100

    added which display

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ document.addEventListener("keydown", (event) => {
 
     let codeDisplay = document.getElementById("codeDisplay");
     let keyDisplay = document.getElementById("keyDisplay");
+    let whichDisplay = document.getElementById("whichDisplay");
 
     codeDisplay.innerHTML = `${event.code}`;
     keyDisplay.innerHTML = `${event.key}`;
+    whichDisplay.innerHTML = `${event.which}`;
+    
 })
